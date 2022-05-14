0f38c5751caf375554e6d8e74f5f8e8cc3575f1d
ilhan <ilhan@DESKTOP-04TMTHU>
2022-05-14 22:36:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyGenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGeneric<string> Product = new MyGeneric<string>();
+
+            Product.Add("ilhan");
+            Product.Add("furkan");
+            Console.WriteLine(Product.Length);
+
+            foreach (var item in Product.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
